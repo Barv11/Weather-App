da8952edc50fbfea6267350c408c537f46347d1a
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-11-30 20:08:03 -0500

    console message

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App/App.js';
 import { BrowserRouter } from 'react-router-dom'
 
+console.log('%cWeatherApp', "color:red;padding:20px;background-color:gray;text-align:center;border-radius:5px")
 ReactDOM.render((
     <BrowserRouter>
         <App />
